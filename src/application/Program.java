a01521d9116bc750510242f81cd7c362a50dc280
vandretorres <vandre.torres@gmail.com>
2020-07-18 18:16:49 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,16 +11,12 @@ public class Program {
 
 	public static void main(String[] args) {
 
-
-		Department depto = new Department(1, "DS");
-		System.out.println(depto);
-
-		
-		Seller sell = new Seller(1, "Vandre", "vandre@", new Date(), 3000.0,  new Department(1, "DS"));
-		
-		System.out.println(sell);
-		
+	
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(1);
+		
+		System.out.println(seller);
 		
 	}
 
