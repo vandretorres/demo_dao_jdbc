b86ebd01d8eb62d63fe04e8e6c295c13ddc051c4
vandretorres <vandre.torres@gmail.com>
2020-07-19 15:23:43 -0300

    create insertSeller method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import db.DB;
@@ -40,6 +41,14 @@ public class Program {
 			
 			System.out.println(obj);
 		}
+		
+		
+		System.out.println();
+		System.out.println("=== TEST 4 :: Seller Create New Seller ======");
+		Seller newSeller = new Seller(null, "Vandre", "vandre@gmail.com", new Date(), 4000.00, department);
+		//sellerDao.insert(newSeller);
+		System.out.println(newSeller);
+		
 		
 				
 		DB.closeConnection();	
